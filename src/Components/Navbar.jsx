739db52f1aeb257b4544db26f9e453f243cc603e
Aakarsh Lohani <134964996+aakarsh5@users.noreply.github.com>
2024-06-25 20:35:31 +0530

    Add files via upload

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+
+const Navbar = () => {
+  const navitem = (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>Practice</a>
+      </li>
+      <li>
+        <a>Course</a>
+      </li>
+      <li>
+        <a>Contact Us</a>
+      </li>
+    </>
+  );
+  return (
+    <div className="container mx-auto md:max-w-screen-xl px-4">
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navitem}
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">Bookie</a>
+        </div>
+        <div className="navbar-end space-x-3">
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">{navitem}</ul>
+          </div>
+          <div className="hidden md:block">
+            <input
+              type="text"
+              placeholder="Search"
+              className="input  input-success w-full max-w-xs"
+            />
+          </div>
+          <label className="cursor-pointer grid place-items-center">
+            <input
+              type="checkbox"
+              value="synthwave"
+              className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
+            />
+            <svg
+              className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <circle cx="12" cy="12" r="5" />
+              <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+            </svg>
+            <svg
+              className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+            </svg>
+          </label>
+          <div>
+            <a className="btn">Login</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
